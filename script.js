301e725d0e586b7ba26c4a69a32a78ee17091682
galangnugrahaaa <galangnugraha007@gmail.com>
2025-12-05 20:47:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,7 @@ function calculate() {
         display.value = "Error";
     }
 }
+function deleteLast() {
+    const display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
